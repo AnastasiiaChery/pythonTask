33c552ad921591d6e533626d975e775ab050765f
AnastasiiaChery <zamorena.nastya@gmail.com>
2021-05-24 18:22:49 +0300

    part3

--- task3.py
+++ task3.py
@@ -176,28 +176,82 @@
 #
 # 21. Write a Python program to find whether a given number (accept from the user) is even or odd, print out an appropriate message to the user. 
 #
+# def check(num):
+#     if num%2==0:
+#         return "Number is even"
+#     else:
+#         return "Number is odd"
+# print(check(7))
+# print(check(4))
+# print(check(-7))
+# print(check(0))
+
 #
-# 22. Write a Python program to count the number 4 in a given list. 
+# 22. Write a Python program to count the number 4 in a given list.
+
+# list=[1, 4, 6, 4, 9, 34, 4, 8, 2, 4]
+# print(list.count(4))
+#
+# 23. Write a Python program to get the n (non-negative integer) copies of the first 2 characters of a given string.
+# Return the n copies of the whole string if the length is less than 2.
+# def substring_copy(str, n):
+#     flen = 2
+#     if flen > len(str):
+#         flen = len(str)
+#     substr = str[:flen]
+#
+#     result = ""
+#     for i in range(n):
+#         result = result + substr
+#     return result
 #
 #
-# 23. Write a Python program to get the n (non-negative integer) copies of the first 2 characters of a given string. Return the n copies of the whole string if the length is less than 2. 
-#
+# print(substring_copy('abcdef', 2))
+# print(substring_copy('p', 3));
 #
 # 24. Write a Python program to test whether a passed letter is a vowel or not. 
 #
+# def is_vowel(char):
+#     all_vowels = 'aeiou'
+#     return char in all_vowels
+# print(is_vowel('c'))
+# print(is_vowel('e'))
+
 #
 # 25. Write a Python program to check whether a specified value is contained in a group of values. 
 # Test Data :
 # 3 -> [1, 5, 8, 3] : True
 # -1 -> [1, 5, 8, 3] : False
-#
+# list=[1, 5, 8, 3]
+# def num_list(list, num):
+#     return num in list
+# print(num_list(list, 5))
+# print(num_list(list, 9))
 #
 #
 # 26. Write a Python program to create a histogram from a given list of integers. 
 #
+# list = [1, 5, 8, 3, 4, 12, 6, 8, 2, 3, 8, 5, 2]
+#
+# def histogram( items ):
+#     for n in items:
+#         output = ''
+#         times = n
+#         while( times > 0 ):
+#           output += '*'
+#           times = times - 1
+#         print(output)
+#
+# histogram(list)
 #
 # 27. Write a Python program to concatenate all elements in a list into a string and return it. 
-#
+# def conc_num(list):
+#     string=''
+#     for i in list:
+#         string=string+str(i)
+#     return string
+# print(conc_num([2, 6, 9, 4, 56]))
+
 #
 # 28. Write a Python program to print all even numbers from a given numbers list in the same order and stop the printing if any numbers that come after 237 in the sequence. 
 # Sample numbers list :
